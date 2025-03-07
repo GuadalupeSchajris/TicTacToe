1de476d3581826e534e35408df253f69a865b69f
Guadalupe Hani Schajris <chikaruwynn@MacBook-Pro-de-Guadalupe.local>
2025-03-07 15:11:27 +0100

    pair 7

--- src/main/java/com/tictctoe/App.java
+++ src/main/java/com/tictctoe/App.java
@@ -6,56 +6,82 @@ public class App
 {
     public static void main( String[] args )
     {
-        String [] tablero = {"_","_","_","_","_","_","_","_","_","_"};
-        int tirada = 0;
-        boolean alguienGano = false;
-    do {    
-        showTablero(tablero);
-        String ficha = tirada%2==0? "X":"O";
-        juegaPlayer(ficha, tablero);
-
-       alguienGano = evaluateWin(tablero);
-       if(alguienGano)System.out.println("Has ganado, player " + ficha + " felicidades!!!");
-        tirada++;
-       } while(!alguienGano);     
+        Scanner lector = new Scanner(System.in);
+        boolean jugarDeNuevo;
+        
+        do {
+            String [] tablero = {"_","_","_","_","_","_","_","_","_"};
+            int tirada = 0;
+            boolean alguienGano = false;
+        
+            do {    
+                showTablero(tablero);
+                String ficha = tirada%2==0? "X":"O";
+                juegaPlayer(ficha, tablero, lector);
     
+                alguienGano = evaluateWin(tablero);
+                if (alguienGano) {
+                    showTablero(tablero);
+                    System.out.println("Has ganado, player " + ficha + " felicidades!!!");
+                    break;
+                }
+                
+                tirada++;
+            } while(tirada < 9 && !alguienGano);
+            
+            if (!alguienGano) {
+                showTablero(tablero);
+                System.out.println("Empate! No hay más movimientos posibles.");
+            }
+            
+            System.out.println("¿Quieres jugar de nuevo? (s/n)");
+            String respuesta = lector.next().toLowerCase();
+            jugarDeNuevo = respuesta.equals("s");
+            
+        } while(jugarDeNuevo);
+        
+        System.out.println("Gracias por jugar! Hasta la próxima.");
+        lector.close();
     }
 
     static void showTablero(String [] tablero) {
-    System.out.println(tablero[0] + " | "+ tablero[1]+" | " + tablero[2]);
-    System.out.println(tablero[3] + " | "+ tablero[4]+" | " + tablero[5]);
-    System.out.println(tablero[6] + " | "+ tablero[7]+" | " + tablero[8]);
-   }
-
-    static void juegaPlayer(String ficha, String[] tablero) {
-    Scanner lector = new Scanner(System.in);
-    System.out.println("¿Cual sera tu movimiento, jugador " + ficha+"?") ;
-    int posicion = lector.nextInt();
-
-    tablero[posicion] = ficha;
-   }
-
-    static boolean evaluateWin(String tablero[]) {
-    if(tablero[0].equals(tablero[1]) && tablero[0].equals(tablero[2]) && !tablero[0].equals("_")) {
-     return true;
-    }else if(tablero[3].equals(tablero[4]) && tablero[3].equals(tablero[5]) && !tablero[3].equals("_")) {
-    return true;
-    }else if(tablero[6].equals(tablero[7]) && tablero[6].equals(tablero[8]) && !tablero[6].equals("_")) {
-        return true;
-    }else if(tablero[0].equals(tablero[3]) && tablero[0].equals(tablero[6]) && !tablero[0].equals("_")) {
-   return true;
-    }else if(tablero[1].equals(tablero[4]) && tablero[1].equals(tablero[7]) && !tablero[1].equals("_")) {
-        return true;
-    }else if(tablero[2].equals(tablero[5]) && tablero[2].equals(tablero[8]) && !tablero[2].equals("_")) {
-        return true;
-    }else if(tablero[0].equals(tablero[4]) && tablero[0].equals(tablero[8]) && !tablero[0].equals("_")) {
-        return true;
-    }else if(tablero[2].equals(tablero[4]) && tablero[2].equals(tablero[6]) && !tablero[2].equals("_")) {
-        return true;
+        System.out.println(tablero[0] + " | "+ tablero[1]+" | " + tablero[2]);
+        System.out.println(tablero[3] + " | "+ tablero[4]+" | " + tablero[5]);
+        System.out.println(tablero[6] + " | "+ tablero[7]+" | " + tablero[8]);
     }
 
-    return false;
+    static void juegaPlayer(String ficha, String[] tablero, Scanner lector) {
+        int posicion;
+        
+        do {
+            System.out.println("¿Cuál será tu movimiento, jugador " + ficha + "?");
+            while (!lector.hasNextInt()) {
+                System.out.println("Entrada no válida. Ingresa un número entre 0 y 8.");
+                lector.next();
+            }
+            posicion = lector.nextInt();
+            
+            if (posicion < 0 || posicion > 8) {
+                System.out.println("Posición inválida. Ingresa un número entre 0 y 8.");
+            } else if (!tablero[posicion].equals("_")) {
+                System.out.println("Casilla ocupada. Elige otra posición.");
+            } else {
+                break;
+            }
+        } while (true);
+        
+        tablero[posicion] = ficha;
+    }
 
-  }
+    static boolean evaluateWin(String tablero[]) {
+        return (tablero[0].equals(tablero[1]) && tablero[0].equals(tablero[2]) && !tablero[0].equals("_")) ||
+               (tablero[3].equals(tablero[4]) && tablero[3].equals(tablero[5]) && !tablero[3].equals("_")) ||
+               (tablero[6].equals(tablero[7]) && tablero[6].equals(tablero[8]) && !tablero[6].equals("_")) ||
+               (tablero[0].equals(tablero[3]) && tablero[0].equals(tablero[6]) && !tablero[0].equals("_")) ||
+               (tablero[1].equals(tablero[4]) && tablero[1].equals(tablero[7]) && !tablero[1].equals("_")) ||
+               (tablero[2].equals(tablero[5]) && tablero[2].equals(tablero[8]) && !tablero[2].equals("_")) ||
+               (tablero[0].equals(tablero[4]) && tablero[0].equals(tablero[8]) && !tablero[0].equals("_")) ||
+               (tablero[2].equals(tablero[4]) && tablero[2].equals(tablero[6]) && !tablero[2].equals("_"));
+    }
 }
 
