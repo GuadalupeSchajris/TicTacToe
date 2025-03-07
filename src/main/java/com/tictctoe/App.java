913f9aea983470ef82547e07ccb355a54ab7fa95
Guadalupe Hani Schajris <chikaruwynn@MacBook-Pro-de-Guadalupe.local>
2025-03-07 12:13:12 +0100

    pair 4

--- src/main/java/com/tictctoe/App.java
+++ src/main/java/com/tictctoe/App.java
@@ -6,22 +6,32 @@ public class App
 {
     public static void main( String[] args )
     {
-        String [] tablero = {"","","","","","","","","",""};
+        String [] tablero = {"_","_","_","_","_","_","_","_","_","_"};
+        int tirada = 0;
+    do {    
         showTablero(tablero);
+        String ficha = tirada%2==0? "X":"O";
+        juegaPlayer("X", tablero);
+        tirada++;
+       } while(true);     
     
     }
 
-   public static void showTablero(String [] tablero) {
+    static void showTablero(String [] tablero) {
     System.out.println(tablero[0] + " | "+ tablero[1]+" | " + tablero[2]);
     System.out.println(tablero[3] + " | "+ tablero[4]+" | " + tablero[5]);
     System.out.println(tablero[6] + " | "+ tablero[7]+" | " + tablero[8]);
    }
 
-   static void juegaPlayer(String ficha, String[] tablero) {
+    static void juegaPlayer(String ficha, String[] tablero) {
     Scanner lector = new Scanner(System.in);
-    System.out.println("Cual sera tu movimiento, jugador " + ficha+"?") ;
+    System.out.println("¿Cual sera tu movimiento, jugador " + ficha+"?") ;
     int posicion = lector.nextInt();
+
+    tablero[posicion] = ficha;
    }
+
+  
 
 
 }
